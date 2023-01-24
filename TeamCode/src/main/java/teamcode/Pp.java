f60b639bec2eddbc88c26959d2385f7a44545270
Eevee5678 <108290506+Eevee5678@users.noreply.github.com>
2023-01-24 16:55:28 -0600

    Update Pp.java

--- TeamCode/src/main/java/teamcode/Pp.java
+++ TeamCode/src/main/java/teamcode/Pp.java
@@ -16,7 +16,8 @@ class Ppbot{
     public DcMotor FLeft = null;
     public DcMotor FRight = null;
     public DcMotor Slider = null;
-    public Servo Take = null;
+    public Servo Take1 = null;
+    public Servo Take2 = null;
 
     HardwareMap map = null;
     public void init(HardwareMap maps) {
@@ -25,8 +26,9 @@ class Ppbot{
         BRight = maps.dcMotor.get("br");
         FLeft = maps.dcMotor.get("fl");
         FRight = maps.dcMotor.get("fr");
-        Take = maps.servo.get("take");
-        Slider = maps.dcMotor.get("s");
+        Take1 = maps.servo.get("grabber");
+        Take2 = maps.servo.get("grabber2");
+        Slider = maps.dcMotor.get("slider");
 
         BLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         BRight.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -39,90 +41,140 @@ class Ppbot{
         FLeft.setPower(0.0);
         FRight.setPower(0.0);
         Slider.setPower(0.0);
-        Take.setPosition(1.0);
+        Slider.setPower(0.0);
 
-        BLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        Slider.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        BLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Slider.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 }
 
 @TeleOp (name = "PowerPlaybot", group = "pp")
 
-public class Pp extends LinearOpMode{
+public class Pp extends LinearOpMode {
     Ppbot robot = new Ppbot();
     double x;
     double y;
     double rx;
-    double Armpos = 1.0;
     double Slidepos = 0.0;
     final double Armspeed = 0.1;
-    final double Slidespeed = 0.5;
+    final double Slidespeed = 1.0;
+    final double rotationScalar = 0.5;
+    final double speedScalar = 0.8;
+    double drivespeed = 0.2;
+    boolean closed = true;
+
+    double pos1 = 0;
+    double pos2 = 0;
 
     @Override
 
-    public void runOpMode(){
+    public void runOpMode() {
         robot.init(hardwareMap);
         telemetry.addData("Say", "Hello");
         telemetry.update();
 
         waitForStart();
 
-        while(opModeIsActive()){
+        //while we balling
+        while (opModeIsActive()) {
+            // y = forward/back x = left strafe/right strafe, rx = rotation
             y = -gamepad1.left_stick_y;
             x = gamepad1.left_stick_x;
             rx = gamepad1.right_stick_x;
 
-            double x2 = x*Math.cos(-Math.PI/4) - y*Math.sin(-Math.PI/4);
-            double y2 = y*Math.cos(-Math.PI/4) + x*Math.sin(-Math.PI/4);
 
-            if (Math.abs(rx) > 0){
-                robot.BLeft.setPower(-rx);
-                robot.BRight.setPower(-rx);
-                robot.FLeft.setPower(rx);
-                robot.FRight.setPower(rx);
+            //if not turning, do a little driving.
+            if (gamepad1.dpad_up)
+                y = 0.2;
+            if (gamepad1.dpad_down)
+                y = -0.2;
+            if (gamepad1.dpad_left)
+                x = -0.3;
+            if (gamepad1.dpad_right)
+                x = 0.3;
+            if (Math.abs(rx) > 0.03) {
+                robot.BLeft.setPower(rotationScalar * -rx);
+                robot.BRight.setPower(rotationScalar * -rx);
+                robot.FLeft.setPower(rotationScalar * rx);
+                robot.FRight.setPower(rotationScalar * -rx);
+            }
+            // if we want to go foward/back do a little motor powering
+            else if (Math.abs(y) >= Math.abs(x) && Math.abs(y) > 0.03) {
+                robot.BLeft.setPower(speedScalar * -y * 0.7); //-
+                robot.BRight.setPower(speedScalar * y * 0.7);
+                robot.FLeft.setPower(speedScalar * y * 0.85); //-
+                robot.FRight.setPower(speedScalar * y); //0
 
             }
-            else if ((gamepad1.right_bumper || (Math.abs(gamepad1.right_trigger) > 0.0)) && Slidepos != 0.0)
-                Slidepos = 0.0;
-            else if (gamepad1.right_bumper)
+            //if we want to go strafing, set a little moter powerfing for strafing
+            else if (Math.abs(x) > (Math.abs(y)) && Math.abs(x) > 0.03) {
+                robot.BLeft.setPower(speedScalar * x * 0.7);
+                robot.BRight.setPower(speedScalar * x * 0.7);
+                robot.FLeft.setPower(speedScalar * x * 0.85);
+                robot.FRight.setPower(speedScalar * -x);
+                //if we dont want to move make sure we dont move
+            } else {
+                robot.BLeft.setPower(0);
+                robot.BRight.setPower(0);
+                robot.FLeft.setPower(0);
+                robot.FRight.setPower(0);
+            }
+            //uppy downy. <- NOT DOWNY madge
+            // this stuff needs changing i think
+            // maybe add stopper
+            Slidepos = 0.0;
+            if (Math.abs(gamepad1.right_trigger) > 0.0) // uppy
                 Slidepos += Slidespeed;
-            else if (Math.abs(gamepad1.right_trigger) > 0.0)
-                Slidepos -= Slidespeed;
-            else if (gamepad1.x)
-                Armpos -= Armspeed;
-            else if (gamepad1.b)
-                Armpos += Armspeed;
-            else if (Math.abs(y2) >= Math.abs(x2)){
-                robot.BLeft.setPower(y2);
-                robot.BRight.setPower(y2);
-                robot.FLeft.setPower(y2);
-                robot.FRight.setPower(y2);
-
+            if (gamepad1.right_bumper) { // stopper
+                Slidepos += Slidespeed / 8;
             }
-            else if (Math.abs(x2) > (Math.abs(y2))){
-                robot.BLeft.setPower(x2);
-                robot.BRight.setPower(x2);
-                robot.FLeft.setPower(x2);
-                robot.FRight.setPower(x2);
-
+            if (Math.abs(gamepad1.left_trigger) > 0.0) {
+                Slidepos -= Slidespeed / 4;
             }
-
+            //open close :)
+            if (gamepad1.x) {
+                closed = true;
+            } else if (gamepad1.b) {
+                closed = false;
+            }
+            if (closed) {
+                robot.Take1.setPosition(0.17);// take 1 closed pos
+                robot.Take2.setPosition(0.47);// take 2 closed pos
+            } else {
+                robot.Take1.setPosition(0.08);// take 1 open pos
+                robot.Take2.setPosition(0.61);// take 2 open pos
+            }
+            //set power and position for grabby and shit
             robot.Slider.setPower(Slidepos);
 
-            Armpos = Range.clip(Armpos, 0.0, 1.0);
-            robot.Take.setPosition(Armpos);
 
-            telemetry.addData("x","%.2f", x);
-            telemetry.addData("y","%.2f", y);
+            //telemetry :nerd_emoji:
+            telemetry.addData("x", "%.2f", x);
+            telemetry.addData("y", "%.2f", y);
+            telemetry.addData("servo1", "%.2f", robot.Take1.getPosition());
+            telemetry.addData("servo2", "%.2f", robot.Take2.getPosition()); // REMEMBER TO CONFIGURE THIS ON PHONE
             telemetry.update();
 
             sleep(50);
-
         }
     }
 
+    public void moveRight() {
+        x = 0.3;
+    }
+
+    public void moveLeft() {
+        x = -0.3;
+    }
+
+    public void moveForward() {
+        y = 0.2;
+    }
+
+    public void moveBackward() {
+        y = -0.2;
+    }
 }

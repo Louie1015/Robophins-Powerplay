251a56fe3a17efc337f6516c1e5ce43b5c72366b
Eevee5678 <108290506+Eevee5678@users.noreply.github.com>
2023-02-04 08:50:33 -0600

    roadrunner complete :D

--- TeamCode/src/main/java/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/teamcode/drive/DriveConstants.java
@@ -32,8 +32,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(0.01934164031));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(2.5, 0.1, 0.2,
+            getMotorVelocityF(2350));
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -45,7 +45,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 13.1; // in
+    public static double TRACK_WIDTH = 13.16; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -66,7 +66,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = 9.62;
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
 

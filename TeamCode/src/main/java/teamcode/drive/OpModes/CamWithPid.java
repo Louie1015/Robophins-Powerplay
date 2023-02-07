085ead51f8315557d8dbb333ea55204e71cbf2ae
AtticusFinch2 <bradyspymiller71@gmail.com>
2023-02-07 12:52:21 -0600

    one cone + park in CamWithPid
    not final draft, needs tuning on trajectory values

--- TeamCode/src/main/java/teamcode/drive/OpModes/CamWithPid.java
+++ TeamCode/src/main/java/teamcode/drive/OpModes/CamWithPid.java
@@ -1,5 +1,7 @@
 package teamcode.drive.OpModes;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -11,6 +13,9 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 
 public class CamWithPid extends LinearOpMode{
     private Cammy camlyn;
+    public static double firstrightdist = 25;
+    public static double firstcycleleft = 35;
+    public static double slidepower = 1.0;
 
 
     OpenCvCamera webcam;
@@ -48,11 +53,11 @@ public class CamWithPid extends LinearOpMode{
             }
         });
         waitForStart();
+
         while(opModeIsActive()) {
-            sleep(5000);
-            //close(); IF WE HAVE START CONE
-            //sleep(500);
-            // TODO: if you want code to run before we check which sleeve is detected(ex:close grabber on startup), run it here
+            sleep(2000);
+
+            // if you want code to run before we check which sleeve is detected(ex:close grabber on startup), run it here
 
 
             if(Cammy.truePath == 1){
@@ -83,17 +88,93 @@ public class CamWithPid extends LinearOpMode{
         }
 
     }
-
+    public Pose2d tempPose;
     public void Path1(){
-        //TODO: make yellow path here
+        drop1();
+        //park into left sector (Yellow)
+        Trajectory yellowTraj1 = mainRobot.trajectoryBuilder(tempPose)
+                .strafeLeft(20)
+                .build();
+        mainRobot.followTrajectory(yellowTraj1);
     }
 
     public void Path2(){
-        //TODO: make blue path here
+        drop1();
+        //park into middle sector (Blue)
+        //should already be in it, not much needs to be done but might need some small adjustments
     }
 
     public void Path3(){
-        //TODO: make magenta path here
+        drop1();
+        //park into right sector (Magenta)
+        Trajectory magentaTraj1 = mainRobot.trajectoryBuilder(tempPose)
+                .strafeRight(20)
+                .build();
+        mainRobot.followTrajectory(magentaTraj1);
     }
+    public void drop1() {
+        Pose2d startPose = new Pose2d(-34, 70, Math.toRadians(-90));
+        mainRobot.setPoseEstimate(startPose);
+
+        Trajectory firstRight = mainRobot.trajectoryBuilder(startPose)
+                .strafeRight(firstrightdist)
+                .build();
+
+        Trajectory firstForward = mainRobot.trajectoryBuilder(firstRight.end())
+                .forward(50)
+                .build();
+
+        Trajectory firstLeft = mainRobot.trajectoryBuilder(firstForward.end())
+                .strafeLeft(firstcycleleft)
+                .build();
+
+        Trajectory creep = mainRobot.trajectoryBuilder(firstLeft.end())
+                .forward(4)
+                .build();
+
+        Trajectory backcreep = mainRobot.trajectoryBuilder(creep.end())
+                .back(5)
+                .build();
+
+        int step = 0;
+        mainRobot.grabber.closeGrabber();
+        mainRobot.pause(500);
+        mainRobot.followTrajectory(firstRight); // GO RIGHT
+        mainRobot.pause(200);
+        step += 1;
+        telemetry.addData("step",":#" + step); telemetry.update();
+        mainRobot.followTrajectory(firstForward); // GO FORWARD
+        mainRobot.pause(200);
+        step += 1;
+        telemetry.addData("step",":#" + step); telemetry.update();
+        mainRobot.followTrajectory(firstLeft); // GO LEFT
+        step += 1;
+        telemetry.addData("step", ":#" + step); telemetry.update();
+        mainRobot.slides.setSlidesPower(1.0); // VERTICAL SLIDE UP
+        mainRobot.pause(2750); // TIME TO GET TO TOP
+        step += 1;
+        telemetry.addData("step",":#" + step); telemetry.update();
+        mainRobot.followTrajectory(creep); // CREEP FORWARDS
+        step += 1;
+        telemetry.addData("step", ":#" + step); telemetry.update();
+        mainRobot.grabber.openGrabber(); //DROP CONE
+        mainRobot.pause(500);
+        step += 1;
+        telemetry.addData("step",":#" + step); telemetry.update();
+        mainRobot.slides.setSlidesPower(-0.3); // GO DOWN WHILE OPEN
+        mainRobot.pause(1800);
+        step += 1;
+        telemetry.addData("step",":#" + step); telemetry.update();
+        mainRobot.slides.setSlidesPower(0.1); //  MAKE SURE SPOOL IS TAUGHT
+        mainRobot.pause(1000);
+        step += 1;
+        telemetry.addData("step",":#" + step); telemetry.update();
+        mainRobot.followTrajectory(backcreep); // go backwards so you can close
+        mainRobot.grabber.closeGrabber();// CLOSE
+        mainRobot.pause(500);
+        step += 1;
+        telemetry.addData("step",":#" + step); telemetry.update();
+        tempPose = backcreep.end();
+    } //this is similar to BlueRight
 
 }
